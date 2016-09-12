7ee2563f56187111c3bdc3fa21c69e1d366c2008
Argel Ramírez Reyes <argel.ramirez@gmail.com>
2016-09-12 21:07:48 +0200

    Implementados los métodos para llenar las matrices y vectores, falta resolver sistema

--- fortran/FortranProject/solveSystem.f90
+++ fortran/FortranProject/solveSystem.f90
@@ -2,14 +2,51 @@
 !Argel Ram'irez Reyes, sept 2016
 module system_solver
   implicit none
-  real, dimension(100,100) :: Coef
-  real, dimension(100)     :: Forces, Displacments
-  integer                  :: ii,jj
+  real                 ::L,h,T
+  integer              ::N 
+  real, allocatable, dimension(:,:) :: Coef
+  real,allocatable, dimension(:)   :: Forces, Displacements,Positions
+  integer              :: ii,jj
+  print*, "Escriba el número de puntos"
+  read*, N
+  h = L/N
+  allocate(Coef(N+1,N+1))
+  allocate(Forces(N+1))
+  allocate(Displacements(N+1))
+  allocate(Positions(N+1))
+
+!aquí se deben llelmar las subrutinas después de alocarse  
+
+  deallocate(Coef)
+  deallocate(Forces)
+  deallocate(Displacements)
+  deallocate(Positions)
 contains
-  function eval_f(x) result (f)
-    real, intent(i) :: x
-    real :: f
-    print*, x
-    f  = -1
-  end function eval_f
+  subroutine fill_matrix
+    Coef(1,1) = 1
+    Coef(N+1,N+1) = 1
+    do ii=2,N
+       Coef(ii,ii-1) = 1
+       Coef(ii,ii) = -2
+       Coef(ii,ii+1) = 1
+    end do
+  end subroutine fill_matrix
+
+
+  subroutine fill_Positions
+    do ii=1,N+1
+       Positions(ii) = h*ii-1
+    end do
+  end subroutine fill_Positions
+
+  subroutine fill_Forces
+    Forces(1) = 0
+    Forces(N+1) = 0
+    do ii=2,N       
+       Forces(ii) =-(h**2)*eval_f(Positions(ii))/T
+    end do
+  end subroutine fill_Forces
+
+  
+
 end module system_solver.f90
