1f531ac45b07a849747a871021f77bf02955b158
Argel Ramirez Reyes <argel.ramirez@gmail.com>
2016-09-13 12:00:53 +0200

    In fortran project: move definitions to main program

--- fortran/FortranProject/call_module.f90
+++ fortran/FortranProject/call_module.f90
@@ -1,11 +1,34 @@
 program call
-use f_function
-    implicit none
-real :: x
-integer :: ii
-do ii = 1,10
-   x = ii
-   print*, x,eval_f(x)
-end do
+  use f_function
+  implicit none
+  real :: x
+  integer :: ii
+
+
+  real                 ::L,h,T
+  integer              ::N 
+  real, allocatable, dimension(:,:) :: Coef
+  real,allocatable, dimension(:)   :: Forces, Displacements,Positions
+  integer              :: ii,jj
+  print*, "Escriba el número de puntos"
+  read*, N
+  h = L/N
+  allocate(Coef(N+1,N+1))
+  allocate(Forces(N+1))
+  allocate(Displacements(N+1))
+  allocate(Positions(N+1))
+
+  !aquí se deben llelmar las subrutinas después de alocarse  
+
+  deallocate(Coef)
+  deallocate(Forces)
+  deallocate(Displacements)
+  deallocate(Positions)
+
+
+  do ii = 1,10
+     x = ii
+     print*, x,eval_f(x)
+  end do
 
 end program call
--- fortran/FortranProject/solveSystem.f90
+++ fortran/FortranProject/solveSystem.f90
@@ -2,25 +2,7 @@
 !Argel Ram'irez Reyes, sept 2016
 module system_solver
   implicit none
-  real                 ::L,h,T
-  integer              ::N 
-  real, allocatable, dimension(:,:) :: Coef
-  real,allocatable, dimension(:)   :: Forces, Displacements,Positions
-  integer              :: ii,jj
-  print*, "Escriba el número de puntos"
-  read*, N
-  h = L/N
-  allocate(Coef(N+1,N+1))
-  allocate(Forces(N+1))
-  allocate(Displacements(N+1))
-  allocate(Positions(N+1))
 
-!aquí se deben llelmar las subrutinas después de alocarse  
-
-  deallocate(Coef)
-  deallocate(Forces)
-  deallocate(Displacements)
-  deallocate(Positions)
 contains
   subroutine fill_matrix
     Coef(1,1) = 1
